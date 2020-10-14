017ec3c48e368a0f7decdf2beedf237ade08671c
Chris Giroux <chris.giroux86@gmail.com>
2020-10-14 19:14:09 -0400

    staging for merge

--- tickets/ticketModel.js
+++ tickets/ticketModel.js
@@ -4,14 +4,20 @@ module.exports = {
   updateTicket,
   findTicketById,
   getTicketsByUserId,
+  getAllTickets,
+  deleteTicketByTicketId,
+  getTicketResponsesByTicketId,
+  createTicketResponse,
 };
 function createTicket(id, ticketInfo) {
-  return db("tickets").insert({
-    submitted_by: id,
-    description: ticketInfo.description,
-    attempted_solutions: ticketInfo.attempted_solutions,
-    priority: ticketInfo.priority_id,
-  });
+  return db("tickets")
+    .insert({
+      submitted_by: id,
+      description: ticketInfo.description,
+      attempted_solutions: ticketInfo.attempted_solutions,
+      priority: ticketInfo.priority_id,
+    })
+    .returning("id");
 }
 
 function updateTicket(id, updates) {
@@ -25,5 +31,44 @@ function findTicketById(id) {
 
 function getTicketsByUserId(id) {
   console.log(id);
-  return db("tickets").where({ submitted_by: id });
+  return (
+    db("tickets")
+      // .leftJoin("ticket_responses", "ticket_responses.ticket_id", "tickets.id")
+      // .leftJoin("users", "users.id", "ticket_responses.user_id")
+      .where({ submitted_by: id })
+  );
+  // .select("description");
+}
+
+function getAllTickets() {
+  return db("tickets")
+    .leftJoin("users", "users.id", "tickets.assigned_to")
+    .select(
+      "users.first_name as assigned_first",
+      "users.last_name as assigned_last",
+      "tickets.description",
+      "tickets.attempted_solutions",
+      "tickets.submitted_by",
+      "tickets.status",
+      "tickets.dept_id",
+      "tickets.priority",
+      "tickets.created_at",
+      "tickets.id as ticket_id"
+    )
+    .orderBy("created_at", "desc");
+  // .select("description", "attempted_solutions", "priority", );
+}
+
+function deleteTicketByTicketId(id) {
+  return db("tickets").where({ id }).del();
+}
+
+function getTicketResponsesByTicketId(ticket_id) {
+  return db("ticket_responses").where({ ticket_id });
+}
+
+function createTicketResponse(ticket_id, response, user_id) {
+  const post = { ticket_id: +ticket_id, user_id, ...response };
+  console.log("POST", post);
+  return db("ticket_responses").insert(post);
 }
--- tickets/ticketRouter.js
+++ tickets/ticketRouter.js
@@ -8,8 +8,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 //POST create new ticket
 router.post("/", (req, res) => {
   const { id } = req.user;
+  const newTicket = { ...req.body, submitted_by: id };
   Tickets.createTicket(id, req.body)
-    .then((ticket) => res.status(201).json(ticket))
+    .then(([ticket]) => res.status(201).json(ticket))
     .catch((err) => res.status(500).json({ error: err }));
 });
 
@@ -21,11 +22,34 @@ router.get("/", (req, res) => {
     .catch((err) => res.status(500).json({ error: err }));
 });
 
+//DELETE a ticket by id
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Tickets.deleteTicketByTicketId(id)
+    .then((deleted) => res.status(204).json(deleted))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+//GET all organization tickets
+router.get("/all", (req, res) => {
+  Tickets.getAllTickets()
+    .then((tickets) => res.status(200).json(tickets))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
 //GET single ticket by ticket id
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   Tickets.findTicketById(id)
     .then((ticket) => res.status(200).json(ticket))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+//GET single ticket responses by ticket id
+router.get("/responses/:ticket_id", (req, res) => {
+  const { ticket_id } = req.params;
+  Tickets.getTicketResponsesByTicketId(ticket_id)
+    .then((responses) => res.status(200).json(responses))
     .catch((err) => res.status(500).json({ error: err }));
 });
 
@@ -53,6 +77,15 @@ router.put("/:id/update", async (req, res) => {
           res.status(500).json({ message: "Could not send.", error: err })
         );
     })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+//POST create new response to ticket with ticket id
+router.post("/:ticket_id/responses", (req, res) => {
+  const { ticket_id } = req.params;
+  console.log("USER", req.user.sub, "MESSAGE", req.body);
+  Tickets.createTicketResponse(ticket_id, req.body, req.user.sub)
+    .then((message) => res.status(201).json(message))
     .catch((err) => res.status(500).json({ error: err }));
 });
 
